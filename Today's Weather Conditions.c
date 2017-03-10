7bcdf6a8b26dd1987a0b293e453207a2c5b16a81
Adam Chimienti <adam_chimienti@hotmail.com>
2017-03-10 14:45:44 -0500

    Today's Weather Conditions

---
+++ Today's Weather Conditions.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	double today_high, today_low, today_condition, average_temp;
+	
+	int i; for (i = 0; i<"%d":i++)
+
+	printf("Enter Today's High: %lf");
+	scanf("%lf", &today_high);
+	printf("Enter Today's Low: %lf");
+	scanf("%lf", &today_low);
+	printf("Enter Today's Conditions: %lf");
+	scanf("%lf", today_condition)
+
+	average_temp = today_high * today_low
+
+	return 0;
+}
